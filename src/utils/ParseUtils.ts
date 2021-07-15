4a9ce17cedaf27c51eaa4490693a95a86a0751f0
Krendeled <danyl1607@gmail.com>
2021-07-15 22:15:22 +0300

    Refactor ParseUtils

--- src/utils/ParseUtils.ts
+++ src/utils/ParseUtils.ts
@@ -3,18 +3,18 @@ abstract class ParseUtils {
         str: string,
         data: { mention?: string; prefix?: string }
     ): string {
+        if (!str) return;
+
         const strings = {
             "{{mention}}": data.mention,
             "{{prefix}}": data.prefix,
         };
 
-        let parsedStr = str;
-
         Object.keys(strings).forEach((key) => {
-            parsedStr = parsedStr.replace(new RegExp(key, "g"), strings[key]);
+            str = str.replace(new RegExp(key, "g"), strings[key]);
         });
 
-        return parsedStr;
+        return str;
     }
 }
 
